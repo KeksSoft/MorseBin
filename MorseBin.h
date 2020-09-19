55c47e178fe163f2f44756729a270a07dd854c91
RegenJoho <63968870+RegenJoho@users.noreply.github.com>
2020-09-19 15:49:51 +0200

    Update MorseBin.h

--- MorseBin.h
+++ MorseBin.h
@@ -6,4 +6,5 @@ extern String MBReceive(int respin);
 extern void MBSendByte(String input,int sendpin);
 extern void MBSend0(int sendpin);
 extern void MBSend1(int sendpin);
+extern byte MBStringToByte(String input);
 #endif
