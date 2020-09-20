4eed9d88cf07fa9bd3ffdf33ed3eaa009edaf775
RegenJoho <63968870+RegenJoho@users.noreply.github.com>
2020-09-20 12:56:35 +0200

    Update MorseBinGPIO.ino

--- examples/MorseBinGPIO/MorseBinGPIO.ino
+++ examples/MorseBinGPIO/MorseBinGPIO.ino
@@ -5,6 +5,9 @@
 // In this array are the digital I/O pins stored,
 // which later can be controlled with the MorseBin protocol
 // you can use it with the MorseBinSerialSend example to controll the Pins over Serial.
+// If you are using a different board than the Arduino UNO,
+// you can change the pins by changing the numbers in the pins-array
+// according to your pins.
 const int pins[8] = {3,4,5,6,7,8,10,11};
 void setup() {
   // Set the pins as an output
@@ -21,20 +24,21 @@ void loop() {
 // e.g. "11111111" will turn on all pins
 // and "00000000" will turn off all lights.
   if(input != ""){
-  if(input.charAt(0)=='1'){digitalWrite(pins[0],HIGH);}
-  else{digitalWrite(pins[0],LOW);}
-  if(input.charAt(1)=='1'){digitalWrite(pins[1],HIGH);}
-  else{digitalWrite(pins[1],LOW);}
-  if(input.charAt(2)=='1'){digitalWrite(pins[2],HIGH);}
-  else{digitalWrite(pins[2],LOW);}
-  if(input.charAt(3)=='1'){digitalWrite(pins[3],HIGH);}
-  else{digitalWrite(pins[3],LOW);}
-  if(input.charAt(4)=='1'){digitalWrite(pins[4],HIGH);}
-  else{digitalWrite(pins[4],LOW);}
-  if(input.charAt(5)=='1'){digitalWrite(pins[5],HIGH);}
-  else{digitalWrite(pins[5],LOW);}
-  if(input.charAt(6)=='1'){digitalWrite(pins[6],HIGH);}
-  else{digitalWrite(pins[6],LOW);}
-  if(input.charAt(7)=='1'){digitalWrite(pins[7],HIGH);}
-  else{digitalWrite(pins[7],LOW);}
+    if(input.charAt(0)=='1'){digitalWrite(pins[0],HIGH);}
+    else{digitalWrite(pins[0],LOW);}
+    if(input.charAt(1)=='1'){digitalWrite(pins[1],HIGH);}
+    else{digitalWrite(pins[1],LOW);}
+    if(input.charAt(2)=='1'){digitalWrite(pins[2],HIGH);}
+    else{digitalWrite(pins[2],LOW);}
+    if(input.charAt(3)=='1'){digitalWrite(pins[3],HIGH);}
+    else{digitalWrite(pins[3],LOW);}
+    if(input.charAt(4)=='1'){digitalWrite(pins[4],HIGH);}
+    else{digitalWrite(pins[4],LOW);}
+    if(input.charAt(5)=='1'){digitalWrite(pins[5],HIGH);}
+    else{digitalWrite(pins[5],LOW);}
+    if(input.charAt(6)=='1'){digitalWrite(pins[6],HIGH);}
+    else{digitalWrite(pins[6],LOW);}
+    if(input.charAt(7)=='1'){digitalWrite(pins[7],HIGH);}
+    else{digitalWrite(pins[7],LOW);}
+  }
 }
